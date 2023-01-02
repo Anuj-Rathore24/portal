fc4217870bc8400e16493a925cb8710a5079ea49
Anuj-Rathore24 <anujsingh2409@gmail.com>
2023-01-02 11:33:02 +0530

    backend for graphql done

--- graphqlEvents.js
+++ graphqlEvents.js
@@ -9,19 +9,18 @@ let event = require("./data/eventData.json");
 let Allrounds = require("./data/roundData.json");
 let allQuestions = require("./data/questionsData.json");
 
-
 // Options Object
 const opt = new GraphQLObjectType({
   name: "optionsInfo",
   description: "",
   fields: () => ({
-    optionsId: {type:GraphQLInt},
-    option: {type:GraphQLString},
+    optionsId: { type: GraphQLInt },
+    option: { type: GraphQLString },
   }),
 });
 
 // Questions Object
-const ques = new GraphQLObjectType({
+const questionObject = new GraphQLObjectType({
   name: "QuestionsInfo",
   description: "",
   fields: () => ({
@@ -33,7 +32,7 @@ const ques = new GraphQLObjectType({
 });
 
 // Round Object
-const round = new GraphQLObjectType({
+const roundObject = new GraphQLObjectType({
   name: "RoundInfo",
   description: "",
   fields: () => ({
@@ -41,18 +40,34 @@ const round = new GraphQLObjectType({
     roundName: { type: GraphQLString },
     roundType: { type: GraphQLString },
     totalScore: { type: GraphQLInt },
-    questions: { type: new GraphQLList(GraphQLInt) },
-    questionsInfo: {
-      type: ques,
-      resolve: (q) => {
-        return allQuestions.find((para) => para.id === q.id);
+    questionIds: { type: new GraphQLList(GraphQLInt) },
+    multipleQuestions: {
+      type: new GraphQLList(questionObject),
+      resolve: (ro) => {
+        let ourList=[];
+        // get question object for every quesion's ids
+        ro.questions.forEach(element => {
+          ourList.push(allQuestions.find(r=>r.questionId===element))
+        });
+        return ourList;
       },
     },
+    question:{
+      type:questionObject,
+      args:{
+        questionId: { type: GraphQLInt },
+      },
+      resolve:(parent,args)=>{
+        // match question with parent's list of question and then question in data list
+        if(parent.questions.find(p=>p===args.questionId)!=undefined) return allQuestions.find(q=>q.questionId===args.questionId)
+        return null;
+      }
+    }
   }),
 });
 
 // Event Object
-const individualEvent = new GraphQLObjectType({
+const eventObject = new GraphQLObjectType({
   name: "event",
   description: "all information about events",
   fields: () => ({
@@ -61,11 +76,27 @@ const individualEvent = new GraphQLObjectType({
     eventDate: { type: GraphQLString },
     roundIds: { type: new GraphQLList(GraphQLInt) },
     participants: { type: new GraphQLList(GraphQLInt) },
-    roundInfo: {
-      type: round,
+    multipleRound: {
+      type: new GraphQLList(roundObject),
       resolve: (event) => {
-        return Allrounds.find((round) => round.roundId === event.eventId);
+        let ourList=[];
+        // get round object for every round's ids
+        event.roundIds.forEach(element => {
+          ourList.push(Allrounds.find(r=>r.roundId===element))
+        });
+        return ourList;
       },
+    },
+    round:{
+      type:roundObject,
+      args:{
+        roundId: { type: GraphQLInt },
+      },
+      resolve:(parent,args)=>{
+        // if round exists in parent event then display list or display null
+        if(parent.roundIds.find(p=>p===args.roundId)!=undefined) return Allrounds.find(r=>r.roundId===args.roundId)
+        return null;
+      }
     },
   }),
 });
@@ -75,12 +106,25 @@ const eventSchema = new GraphQLSchema({
     name: "EventQueries",
     description: "Main Query Object",
     fields: () => ({
-      event: {
-        type: new GraphQLList(individualEvent),
+      multipleEvent: {
+        type: new GraphQLList(eventObject),
+        description:"For all events",
         resolve: () => event,
+      },
+      event: {
+        type: eventObject,
+        description: "a single event information",
+        args: {
+          eventId: { type: GraphQLInt },
+        },
+        resolve: (parent, args) =>
+          event.find((e) => e.eventId === args.eventId),
       },
     }),
   }),
 });
 
 module.exports = { eventSchema };
+
+
+// schema : Events object->round object ->question object ->options object (-> : means references!)
